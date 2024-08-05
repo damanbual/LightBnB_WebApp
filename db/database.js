f7203293575a6871469a111ebdad38d01207aaeb
Daman Bual <damanbual@gmail.com>
2024-08-05 14:28:44 -0700

    added databse connection

--- db/database.js
+++ db/database.js
@@ -1,3 +1,23 @@
+
+// Connecting to Database
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'damanbual',
+  password: '',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+
+// Verifing Connection
+pool.query(`SELECT title FROM properties LIMIT 10;`)
+  .then(response => {
+    console.log(response.rows);
+  })
+  .catch(err => {
+    console.error('Connection error', err.stack);
+  });
+
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
@@ -59,12 +79,17 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool.query(`
+    SELECT * FROM properties
+    LIMIT $1;
+  `, [limit])
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => {
+      console.error('Query error', err.stack);
+    });
 };
 
 /**
