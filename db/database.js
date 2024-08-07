5a2cd71c20de591201bde1840193b3731c4cf36b
Daman Bual <damanbual@gmail.com>
2024-08-06 18:02:43 -0700

    Refactor getAllReservations to query real reservation data

--- db/database.js
+++ db/database.js
@@ -103,7 +103,21 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+    SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `;
+  const queryParams = [guest_id, limit];
+
+  return pool.query(queryString, queryParams)
+    .then((result) => result.rows)
+    .catch((err) => console.error('query error', err.stack));
 };
 
 /// Properties
