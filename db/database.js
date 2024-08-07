2c7a018327c5b7ffd736b4105d72510c20420a4f
Daman Bual <damanbual@gmail.com>
2024-08-06 17:23:59 -0700

    Refactored user functions to use database queries

--- db/database.js
+++ db/database.js
@@ -28,15 +28,23 @@ const users = require("./json/users.json");
  * @param {String} email The email of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  const queryString = `
+    SELECT * 
+    FROM users 
+    WHERE email = $1;
+  `;
+  const values = [email];
+
+  return pool.query(queryString, values)
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      }
+      return null;
+    })
+    .catch((err) => console.error('query error', err.stack));
 };
 
 /**
@@ -44,21 +52,48 @@ const getUserWithEmail = function (email) {
  * @param {string} id The id of the user.
  * @return {Promise<{}>} A promise to the user.
  */
+
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  const queryString = `
+    SELECT * 
+    FROM users 
+    WHERE id = $1;
+  `;
+  const values = [id];
+
+  return pool.query(queryString, values)
+    .then((result) => {
+      if (result.rows.length > 0) {
+        return result.rows[0];
+      }
+      return null;
+    })
+    .catch((err) => console.error('query error', err.stack));
 };
+
 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
+
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  const queryString = `
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+  `;
+  const values = [user.name, user.email, user.password];
+
+  return pool.query(queryString, values)
+    .then((result) => {
+      return result.rows[0];
+    })
+    .catch((err) => console.error('query error', err.stack));
 };
+
+
 
 /// Reservations
 
